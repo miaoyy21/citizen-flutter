6d4c7b53e9a3e6350903316afae594155d3d8c40
MiaoJingyi <miaojingyi86@gmail.com>
2024-03-07 03:09:53 +0800

    .

--- lib/component/test.dart
+++ lib/component/test.dart
@@ -2,16 +2,26 @@ import 'package:citizen/index.dart';
 
 class TestComponent extends PositionComponent
     with HasGameReference<CitizenGame> {
-  TestComponent({super.position, super.size})
+  final Color debugColor;
+
+  TestComponent({super.position, super.size, required this.debugColor})
       : super(anchor: Anchor.bottomLeft);
 
   @override
   FutureOr<void> onLoad() {
+    // 示例：
+    // final min1 = other.frame.exposeHead.first.min.toGlobal(p1);
+    // final max1 = other.frame.exposeHead.first.max.toGlobal(p1);
+    //
+    // final ttt = TestComponent(
+    //     position: Vector2(min1.x, min1.y),
+    //     size: Vector2(max1.x - min1.x, min1.y - max1.y));
+    // world.add(ttt);
+
     add(
       RectangleHitbox()
         ..debugMode = true
-        ..isSolid = true
-        ..debugColor = Colors.redAccent,
+        ..debugColor = debugColor,
     );
   }
 }
