96a78bbb631aef1ef1a014d7fd0c70688e0febd9
MiaoJingyi <miaojingyi86@gmail.com>
2024-03-26 05:24:09 +0800

    .

--- lib/routes/stage.dart
+++ lib/routes/stage.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:math';
 
 import '../index.dart';
@@ -24,8 +23,6 @@ class Stage extends Component with TapCallbacks, HasGameReference<CitizenGame> {
     world.add(SpriteComponent.fromImage(await Flame.images.load("129.png"),
         anchor: Anchor.bottomLeft));
     debugPrint("onLoad game.world.children.length => ${world.children.length}");
-
-    FlameAudio.bgm.play(fileName)..
 
     final stage = SpriteComponent.fromImage(
         await Flame.images.load("$name.png"),
